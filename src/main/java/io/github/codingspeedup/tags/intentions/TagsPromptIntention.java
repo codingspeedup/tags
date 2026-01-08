3b4b3c5be315146913361bbe50911585741b149f
coding.speedup <coding.speedup@gmail.com>
2026-01-08 12:54:55 +0200

    icons and labels

--- src/main/java/io/github/codingspeedup/tags/intentions/TagsPromptIntention.java
+++ src/main/java/io/github/codingspeedup/tags/intentions/TagsPromptIntention.java
@@ -7,6 +7,8 @@ import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.util.Iconable;
 import com.intellij.psi.PsiFile;
 import io.github.codingspeedup.tags.MyMessageBundle;
 import io.github.codingspeedup.tags.actions.TagsGroup;
@@ -14,7 +16,11 @@ import io.github.codingspeedup.tags.integration.groovy.ToolboxManagerService;
 import io.github.codingspeedup.tags.utils.FileTypeModel;
 import org.jetbrains.annotations.NotNull;
 
-public class TagsPromptIntention implements IntentionAction, HighPriorityAction {
+import javax.swing.*;
+
+public class TagsPromptIntention implements IntentionAction, HighPriorityAction, Iconable {
+
+    private static final Icon MY_ICON = IconLoader.getIcon("/META-INF/processIcon.svg", TagsPromptIntention.class);
 
     @Override
     public @NotNull String getFamilyName() {
@@ -24,6 +30,11 @@ public class TagsPromptIntention implements IntentionAction, HighPriorityAction 
     @Override
     public @NotNull String getText() {
         return MyMessageBundle.message("action.Execute.text");
+    }
+
+    @Override
+    public Icon getIcon(int flags) {
+        return MY_ICON;
     }
 
     @Override
